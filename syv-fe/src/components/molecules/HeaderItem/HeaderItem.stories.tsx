1fe3fe7698053f92d546b7dcaf45cc936cd61848
haiduongdana <haiduongdana@gmail.com>
2023-06-26 16:13:50 +0700

    HeaderItem testing

--- syv-fe/src/components/molecules/HeaderItem/HeaderItem.stories.tsx
+++ syv-fe/src/components/molecules/HeaderItem/HeaderItem.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import HeaderItem from "./HeaderItem";
+import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { expect, jest } from '@storybook/jest';
 
 const meta = {
   title: "molecules/HeaderItem",
@@ -10,6 +12,14 @@ const meta = {
     link: "/",
     title: "Title",
   },
+  play: async ({ canvasElement, args, step }) => {
+    const canvas = within(canvasElement);
+    const headerItem: HTMLButtonElement = await canvas.findByRole('button');
+
+    await step('Header title', async () => {
+      expect(headerItem.textContent).toBe(args.title)
+    });
+  }
 } satisfies Meta<typeof HeaderItem>;
 
 export default meta;
